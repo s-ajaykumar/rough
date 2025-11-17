8df5552d86340cd80ceca265e10d093d31cd1ba2
s-ajaykumar <mrbraver2003@gmail.com>
2025-11-17 18:54:34 +0530

    Built AlexNet and trained it using MNIST

---
+++ AlexNet.py
@@ -0,0 +1,88 @@
+# Import Libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import transforms
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+# Config Variables
+batch_size = 60
+lr = 1e-3
+epochs = 10
+
+# Data Collection
+train_dataset = datasets.MNIST(root = 'datasets/', train = True, download = True, transform = transforms.ToTensor())
+test_dataset = datasets.MNIST(root = 'datasets/', train = False, download = True, transform = transforms.ToTensor())
+    
+# Data Preprocessing
+train_dataloader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
+test_dataloader = DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = True)
+
+def show(img):
+    img = img.numpy()
+    img = img.transpose(1, 2, 0)
+    plt.imshow(img)
+    plt.show()
+img, _ = next(iter(train_dataloader))
+#show(torchvision.utils.make_grid(img))        
+    
+# Model Building
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size = 3),  # 26*26*32
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),                 # 13*13*32
+            
+            nn.Conv2d(32, 64, kernel_size = 3),  # 11*11*64
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),                  # 5*5*64
+            
+            nn.Flatten(),
+            nn.Linear(5*5*64, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        )
+        
+    def forward(self, x, targets = None):
+        logits = self.cnn(x)    # B, 10
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss
+    
+    def classify(self, x):
+        logits, _ = self(x)
+        probs = F.softmax(logits, dim = -1)
+        ix = torch.multinomial(probs, num_samples = 1)
+        return ix
+    
+model = CNN()
+print(f"Model Parameters: {sum([p.nelement() for p in model.parameters()])/1e6:.3f} M")
+
+# Model Training
+optimizer = torch.optim.AdamW(model.parameters(), lr = lr)
+for _ in range(epochs):
+    losses = []
+    for x, y in train_dataloader:
+        _, loss = model(x, y)
+        losses.append(loss.item())
+        optimizer.zero_grad(set_to_none = True)
+        loss.backward()
+        optimizer.step()
+    print("train_loss: ", sum(losses)/len(losses), end = "\t")
+    losses = []
+    model.eval()
+    for x, y in test_dataloader:
+        _, loss = model(x, y)
+        losses.append(loss.item())
+    print("test_loss: ", sum(losses)/len(losses))
+    model.train()
+    
+        
+
